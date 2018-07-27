caf841e180a91b1648c71263ba6a9597de1648b2
James Campbell <jcampbell@parrylabs.com>
2018-07-27 19:00:29 -0400

    added cargo setup for folders

---
+++ tcp_server/target/debug/deps/tcp_server-54871e52032b7a86.d
@@ -0,0 +1,5 @@
+/Users/jamescampbell/projects/rust-examples/tcp_server/target/debug/deps/tcp_server-54871e52032b7a86: src/main.rs
+
+/Users/jamescampbell/projects/rust-examples/tcp_server/target/debug/deps/tcp_server-54871e52032b7a86.d: src/main.rs
+
+src/main.rs:
---
+++ tcp_server/target/debug/tcp_server.d
@@ -0,0 +1,1 @@
+/Users/jamescampbell/projects/rust-examples/tcp_server/target/debug/tcp_server.dSYM: /Users/jamescampbell/projects/rust-examples/tcp_server/src/main.rs
